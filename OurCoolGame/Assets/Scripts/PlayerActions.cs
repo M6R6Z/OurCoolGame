2895670b918bcfbf1a3aceb39a2ce7f7a418a4da
M6R6Z <mironj15@gmail.com>
2023-07-16 16:56:59 +0300

    Начал делать механику строительства
    Some changes with building

--- OurCoolGame/Assets/Scripts/PlayerActions.cs
+++ OurCoolGame/Assets/Scripts/PlayerActions.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
-   [SerializeField] private GameObject ObjectToSpawn;
+    [SerializeField] private float movementSpeed = 10.0f;
+    [SerializeField] private GameObject ObjectToSpawn;
+    public static float vertical, horizontal;
+    
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P))
         {
             Instantiate(ObjectToSpawn, new Vector3Int(0, 0, 0), new Quaternion(0, 0, 0, 0));
         }
+
+        horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
+        vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+
+        
     }
 }
